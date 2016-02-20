4f64796521e1ca0bc7e4aeef250040f204ecf4b9
Adam Hartz <ahartz1@gmail.com>
2016-02-19 22:44:58 -0500

    Complete #13 Large Sum

---
+++ test_e13large_sum.js
@@ -0,0 +1,14 @@
+var test = require('mocha').it,
+    expect = require('chai').expect,
+    assert = require('chai').assert,
+    main = require('./e13large_sum.js');
+
+const TEST_NUMS = `
+123456789012
+000000000000`;
+
+test('largeSum', function() {
+  expect(main.largeSum).is.a('function');
+  // expect(main.largeSum(TEST_NUMS).is.a('number'));
+  expect(main.largeSum(TEST_NUMS)).is.deep.equal(1234567890);
+});
