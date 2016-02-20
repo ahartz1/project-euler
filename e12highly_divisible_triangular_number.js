c3b4e8f15cbb7bb8471d894b589679ca2aa03fc7
Adam Hartz <ahartz1@gmail.com>
2016-02-19 21:03:34 -0500

    Complete #12 Highly Divisible Triangular Number

---
+++ e12highly_divisible_triangular_number.js
@@ -0,0 +1,32 @@
+;(function() {
+  exports.triangularDivisors = triangularDivisors;
+
+  function triangularDivisors(n) {
+    // Return first triangular number with *more than* n divisors
+    // Triangular number is a number in sequence of 1 + 2 + 3 + 4 + 5 + ...
+
+    // Strategy for solving: keep track of current triangular number, check for
+    // number of divisors, if less than n, produce the next triangular number to
+    // check and repeat
+
+    var checkNumFactors = function (curr_tri) {
+      var numFactors = 0;
+      for (var i = 1; i <= curr_tri; i++) {
+        if (curr_tri % i === 0) {
+          numFactors++;
+        }
+      }
+      return numFactors <= n;
+    };
+
+    var index = 1,
+        tri = 1;
+    while (checkNumFactors(tri)) {
+      index++;
+      tri += index;
+    }
+    return tri;
+  }  // end of triangularDivisors
+  // console.log(triangularDivisors(500));
+  console.log('For n of 500, answer = 76576500 (took ~30 min to calculate!)')
+})();
---
+++ test_e12highly_divisible_triangular_number.js
@@ -0,0 +1,14 @@
+var test = require('mocha').it,
+    expect = require('chai').expect,
+    assert = require('chai').assert,
+    main = require('./e12highly_divisible_triangular_number.js');
+
+test('triangularDivisors', function() {
+  expect(main.triangularDivisors).is.a('function');
+  expect(main.triangularDivisors(2)).is.a('number');
+  expect(main.triangularDivisors(2)).is.deep.equal(6);
+  expect(main.triangularDivisors(4)).is.deep.equal(28);
+  expect(main.triangularDivisors(5)).is.deep.equal(28);
+  assert.deepEqual(main.triangularDivisors(2), 6);
+  assert.deepEqual(main.triangularDivisors(4), 28);
+});
