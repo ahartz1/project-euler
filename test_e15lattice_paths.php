0e195ab3f2af63212be0f38506623da6981b9490
Adam Hartz <ahartz1@gmail.com>
2016-02-22 22:02:00 -0500

    Fix euler variable in tests 2 and 3

--- test_e15lattice_paths.php
+++ test_e15lattice_paths.php
@@ -16,12 +16,12 @@ class TestNumLatticePaths extends PHPUnit_Framework_TestCase
 
     public function testKnownGrid3x3()
     {
-        $this->assertEquals($euler->numLatticePaths(3, 3), 20);
+        $this->assertEquals($this->euler->numLatticePaths(3, 3), 20);
     }
 
     public function testKnownGrid2x3()
     {
-        $this->assertEquals($euler->numLatticePaths(2, 3), 10);
+        $this->assertEquals($this->euler->numLatticePaths(2, 3), 10);
     }
 }
 
