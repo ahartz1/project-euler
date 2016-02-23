40fde8e44835b383e49e5a2a46ad806915e0f38a
Adam Hartz <ahartz1@gmail.com>
2016-02-22 21:54:39 -0500

    Improve speed with dictionary cache, improve test structure

--- e15lattice_paths.php
+++ e15lattice_paths.php
@@ -3,35 +3,53 @@
 // Find the number of paths from the top left to the bottom right of a grid
 // of dimension width x height.
 
-function numLatticePaths($width, $height)
+class e15Lattice_Paths
 {
-     // Recursive definition is going to work well here. If we have a
-     // column or row of dimension 1, the number of paths is the number of
-     // edges in the other dimension.
+    public $pathHolder = array();
 
-     // Base case 1
-     if ($width === 1) {
-         return $height + 1;
-     }
+    public function numLatticePaths($width, $height)
+    {
+         // Recursive definition is going to work well here. If we have a
+         // column or row of dimension 1, the number of paths is the number of
+         // edges in the other dimension. Because the processing time is long,
+         // whenever a result is calculated, it is placed into a map for later
+         // reference.
+    
+         // Base case 1
+         if ($width === 1) {
+             return $height + 1;
+         }
+    
+         // Base case 2
+         if ($height === 1) {
+             return $width + 1;
+         }
 
-     // Base case 2
-     if ($height === 1) {
-         return $width + 1;
-     }
-
-     // Recursive case: reduce by one column and sum resulting grids
-     $reducedWidth = $width - 1;
-     $ret = 1;
-     for ($i = 0; $i < $height; $i++) {
-         $ret += numLatticePaths($reducedWidth, $height - $i);
-     }
-
-     return $ret;
+         // Check to see if answer is already in class dictionary
+         if (key_exists($width . $height, $this->pathHolder)) {
+             return $this->pathHolder[$width . $height];
+         } elseif (key_exists($height . $width, $this->pathHolder)) {
+             return $this->pathHolder[$height . $width];
+         }
+    
+         // Recursive case: reduce by one column and sum resulting grids
+         $reducedWidth = $width - 1;
+         $ret = 1;
+         for ($i = 0; $i < $height; $i++) {
+             $ret += $this->numLatticePaths($reducedWidth, $height - $i);
+         }
+    
+         // Save result into dictionary for future calls
+         $this->pathHolder[$width . $height] = $ret;
+         return $ret;
+    }
 }
 
-echo "This will take a long time..., eventually it will answer: 137846528820";
-$eulerWidth = 20;
-$eulerHeight = 20;
-echo "Number of lattice paths in " . $eulerWidth . "x" . $eulerHeight
-    . " grid: " . numLatticePaths($eulerWidth, $eulerHeight);
+if (!count(debug_backtrace())) {
+    $eulerWidth = 20;
+    $eulerHeight = 20;
+    $euler = new e15Lattice_Paths();
+    echo "Number of lattice paths in " . $eulerWidth . "x" . $eulerHeight
+        . " grid: " . $euler->numLatticePaths($eulerWidth, $eulerHeight) . "\n";
+}
 
--- test_e15lattice_paths.php
+++ test_e15lattice_paths.php
@@ -3,41 +3,25 @@
 
 class TestNumLatticePaths extends PHPUnit_Framework_TestCase
 {
+    protected $euler;
+
+    protected function setUp() {
+        $this->euler = new e15Lattice_Paths();
+    }
+
     public function testKnownGrid2x2()
     {
-        $this->assertEquals(numLatticePaths(2, 2), 6);
+        $this->assertEquals($this->euler->numLatticePaths(2, 2), 6);
     }
 
     public function testKnownGrid3x3()
     {
-        $this->assertEquals(numLatticePaths(3, 3), 20);
+        $this->assertEquals($euler->numLatticePaths(3, 3), 20);
     }
-}
 
-
-function numLatticePaths($width, $height)
-{
-     // Recursive definition is going to work well here. If we have a
-     // column or row of dimension 1, the number of paths is the number of
-     // edges in the other dimension.
-
-     // Base case 1
-     if ($width === 1) {
-         return $height + 1;
-     }
-
-     // Base case 2
-     if ($height === 1) {
-         return $width + 1;
-     }
-
-     // Recursive case: reduce by one column and sum resulting grids
-     $reducedWidth = $width - 1;
-     $ret = 1;
-     for ($i = 0; $i < $height; $i++) {
-         $ret += numLatticePaths($reducedWidth, $height - $i);
-     }
-
-     return $ret;
+    public function testKnownGrid2x3()
+    {
+        $this->assertEquals($euler->numLatticePaths(2, 3), 10);
+    }
 }
 
